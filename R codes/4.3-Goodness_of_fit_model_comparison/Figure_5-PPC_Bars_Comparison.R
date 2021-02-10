42395eb9d165e70e4d3de73e073be3051db71d41
Vasilis-Palaskas <vasilis@palaskas.me>
2021-02-10 22:14:01 +0200

    Same scaling in y-axis of Figure 5

--- R codes/4.3-Goodness_of_fit_model_comparison/Figure_5-PPC_Bars_Comparison.R
+++ R codes/4.3-Goodness_of_fit_model_comparison/Figure_5-PPC_Bars_Comparison.R
@@ -26,10 +26,16 @@ source(file.choose())#\R Codes\4.3- Goodness of fit model comparison\In Sample D
 	#load(file.choose())#/Output/MAD Comparison (In and Out of Sample)/In-Sample/	load(file.choose())#/Output/MAD/In sample Predicted differences (ZDTS)(Multinomial values)
 #multi_pred_differences_out
 
+limits <- c(0, 132)
+breaks <- seq(limits[1], limits[2], by=12)
 
+# assign common axis to both plots
+# p1.common.y <- p1 + scale_y_continuous(limits=limits, breaks=breaks)
+# p2.common.y <- p2 + scale_x_continuous(limits=limits, breaks=breaks)
 plot1<-ppc_bars(dataList_new_final$dif_sets,multi_pred_differences_out_ordered,prob = 0.95, width = 0.9, size = 1, fatten = 3,
-                freq = TRUE)+labs(x="Set difference")+scale_x_discrete(limits=c("-3","-2","-1","1","2","3"))+labs(x="Set Difference",y="Number of games")+ggtitle("Ordered-Multinomial")+
-  theme(axis.text.x = element_text( size = 23, angle = 90, hjust = .5, vjust = .5),
+                freq = TRUE)+labs(x="Set difference")+scale_y_continuous(limits=limits, breaks=breaks)+
+  scale_x_discrete(limits=c("-3","-2","-1","1","2","3"))+labs(x="Set Difference",y="Number of games")+ggtitle("Ordered-Multinomial")+
+  theme(axis.text.x = element_text( size = 23, angle = 0, hjust = .5, vjust = .5),
         axis.text.y = element_text( size = 23, angle = 0, hjust = 1, vjust = 0),  
         axis.title.x = element_text( size = 25, angle = 0, hjust = .5, vjust = 0),
         axis.title.y = element_text( size = 25, angle = 90, hjust = .5, vjust= 0),
@@ -38,8 +44,9 @@ plot1<-ppc_bars(dataList_new_final$dif_sets,multi_pred_differences_out_ordered,p
 
 
 plot2<-ppc_bars(dataList_new_final$dif_sets,multi_pred_differences_out,prob = 0.95, width = 0.9, size = 1, fatten = 3,
-                freq = TRUE)+labs(x="Set difference")+labs(x="Set Difference",y="")+scale_x_discrete(limits=c("-3","-2","-1","1","2","3"))+ggtitle("ZDTS")+
-  theme(axis.text.x = element_text( size = 23, angle = 90, hjust = .5, vjust = .5),
+                freq = TRUE)+labs(x="Set difference")+labs(x="Set Difference",y="")+scale_y_continuous(limits=limits, breaks=breaks)+
+  scale_x_discrete(limits=c("-3","-2","-1","1","2","3"))+ggtitle("ZDTS")+
+  theme(axis.text.x = element_text( size = 23, angle = 0, hjust = .5, vjust = .5),
         axis.text.y = element_text( size = 23, angle = 0, hjust = 1, vjust = 0),  
         axis.title.x = element_text( size = 25, angle = 0, hjust = .5, vjust = 0),
         axis.title.y = element_text( size = 25, angle = 90, hjust = .5, vjust= 0),
